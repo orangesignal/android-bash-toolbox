c6de84b8459f6748f3ef5f9fdf97127d3a80b180
Koji Sugisawa <sugisawa@kca.biglobe.ne.jp>
2014-12-29 18:11:14 +0900

    Update android-sdk-maven-deploy.bash

--- android-sdk-maven-deploy.bash
+++ android-sdk-maven-deploy.bash
@@ -18,15 +18,10 @@ DEPLOYER_HOME="${PWD}/maven-android-sdk-deployer"
 
 
 shell_dir=$(dirname ${BASH_SOURCE:-$0})
-
-set +e
-source "$shell_dir/android-sdk-update.bash"
-if [ $? != 0 ]; then
-	exit $?
-fi
-set -e
+source "$shell_dir/android-sdk-download.bash"
 
 echo "Validating Android SDK - $ANDROID_HOME"
+build_tools_dir="$ANDROID_HOME/build-tools"
 filter=''
 
 # sources
@@ -48,6 +43,18 @@ results=$("$ANDROID_HOME/tools/android" list sdk --extended --no-ui)
 list=$(echo "$results" | grep 'id:' | cut -d \" -f2)
 for item in ${list}; do
 	case "$item" in
+		build-tools-* )
+			filter="${filter},${item}"
+			;;
+		platform-tools )
+			filter="${filter},${item}"
+			;;
+		android-* )
+			filter="${filter},${item}"
+			;;
+		addon-* )
+			filter="${filter},${item}"
+			;;
 		extra-android-* )
 			filter="${filter},${item}"
 			;;
@@ -61,21 +68,36 @@ filter=${filter#,}
 if test "$filter" != ''; then
 	echo 'Update Android SDK...'
 	"$ANDROID_HOME/tools/android" update sdk --filter $filter --no-ui --all
+
+	if [[ -d "$build_tools_dir" ]]; then
+		for ver in $(ls -1r "$build_tools_dir"); do
+			rm -f "$ANDROID_HOME/tools/dexdump"
+			ln -s "$build_tools_dir/$ver/dexdump" "$ANDROID_HOME/tools/dexdump"
+			rm -f "$ANDROID_HOME/tools/zipalign"
+			ln -s "$build_tools_dir/$ver/zipalign" "$ANDROID_HOME/tools/zipalign"
+			break
+		done
+	fi
 fi
 
 # Apache Maven
 if [[ ! -d "$M2_HOME" ]]; then
 	echo 'Downloading Apache Maven'
-	status=$(curl --ipv4 --location --output "$PWD/apache-maven-3.1.1-bin.zip" --silent --write-out '%{http_code}' http://archive.apache.org/dist/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.zip)
+	ver='3.1.1'
+	url="http://archive.apache.org/dist/maven/maven-3/${ver}/binaries/apache-maven-${ver}-bin.zip"
+	archive_path="${PWD}/${url##*/}"
+	exdir="${PWD}/$(echo "${url##*/}" | sed 's/-bin.zip//g')"
+	status=$(curl --ipv4 --location --output "$archive_path" --silent --write-out '%{http_code}' http://archive.apache.org/dist/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.zip)
 	if test "$status" != '200'; then
 		echo "ERROR - Download failed: $status"
 		exit 1
 	fi
 	set +e
-	unzip "$PWD/apache-maven-3.1.1-bin.zip"
+	unzip "$archive_path"
 	set -e
-	if test "$PWD/apache-maven-3.1.1" != "$M2_HOME"; then
-		mv "$PWD/apache-maven-3.1.1" "$M2_HOME"
+	if test "$exdir" != "$M2_HOME"; then
+		rm -rf "$M2_HOME"
+		mv "$exdir" "$M2_HOME"
 	fi
 fi
 
@@ -83,13 +105,143 @@ fi
 if [[ ! -d "$DEPLOYER_HOME" ]]; then
 	set +x
 	git --version
-	git clone --single-branch -b master https://github.com/simpligility/maven-android-sdk-deployer "$DEPLOYER_HOME"
+	git clone --depth 1 --single-branch -b master https://github.com/simpligility/maven-android-sdk-deployer "$DEPLOYER_HOME"
 	set -x
 fi
 
-# Local deploy
-cd "$DEPLOYER_HOME"
-"$M2_HOME/bin/mvn" -e install
+# Local repository deploy
+#"$M2_HOME/bin/mvn" -e -f "$DEPLOYER_HOME/pom.xml" install
 
-# In-House deploy
+# In-House repository deploy
+# $HOME/.m2/settings.xml - Sonatype Nexus OSS example
+# <servers>
+#   <server>
+#     <id>nexus</id>
+#     <username>admin</username>
+#     <password>admin123</password>
+#   </server>
+# </servers>
+#
+deploy_url='http://localhost:8081/nexus/content/repositories/thirdparty/'
+server_id='nexus'
+
+if test "$deploy_url" = ''; then
+	exit 0
+fi
+
+# Android Support Repository
+repo_path="$ANDROID_HOME/extras/android/m2repository"
+escaped_repo_path=$(echo "$repo_path" | sed 's/\//\\\//g')
+for pom in $(find "$repo_path" -type f -name "*.pom"); do
+	prefix="${pom%.*}"
+	groupId=$(xpath "$pom" "//project/groupId/text()" 2>/dev/null)
+	artifactId=$(xpath "$pom" "//project/artifactId/text()" 2>/dev/null)
+	version=$(xpath "$pom" "//project/version/text()" 2>/dev/null)
+	packaging=$(xpath "$pom" "//project/packaging/text()" 2>/dev/null)
+	if test "$packaging" = ''; then
+		packaging='jar'
+	fi
+
+	# artifact
+	file="${prefix}.${packaging}"
+	url="${deploy_url%/}$(echo "$file" | sed "s/${escaped_repo_path}//g")"
+	status=$(curl --ipv4 --location --output /dev/null --silent --write-out '%{http_code}' $url)
+	case "$status" in
+		2?? )
+			;;
+		3?? )
+			;;
+		404 )
+			"$M2_HOME/bin/mvn" -e deploy:deploy-file \
+				-DpomFile="$pom" \
+				-Dfile="$file" \
+				-Durl=$deploy_url -DrepositoryId=$server_id
+			;;
+		* )
+			echo "ERROR - $file upload failed: $status"
+			exit 1
+			;;
+	esac
+
+	# javadoc.jar
+	file="${prefix}-javadoc.jar"
+	if [[ -f "$file" ]]; then
+		url="${deploy_url%/}$(echo "$file" | sed "s/${escaped_repo_path}//g")"
+		status=$(curl --ipv4 --location --output /dev/null --silent --write-out '%{http_code}' $url)
+		case "$status" in
+			2?? )
+				;;
+			3?? )
+				;;
+			404 )
+				"$M2_HOME/bin/mvn" -e deploy:deploy-file \
+					-DgroupId=$groupId -DartifactId=$artifactId -Dversion=$version \
+					-Dfile="$file" -Dpackaging=jar -Dclassifier=javadoc -DgeneratePom=false \
+					-Durl=$deploy_url -DrepositoryId=$server_id
+				;;
+			* )
+				echo "ERROR - $file upload failed: $status"
+				exit 1
+				;;
+		esac
+	fi
+
+	# sources.jar
+	file="${prefix}-sources.jar"
+	if [[ -f "$file" ]]; then
+		url="${deploy_url%/}$(echo "$file" | sed "s/${escaped_repo_path}//g")"
+		status=$(curl --ipv4 --location --output /dev/null --silent --write-out '%{http_code}' $url)
+		case "$status" in
+			2?? )
+				;;
+			3?? )
+				;;
+			404 )
+				"$M2_HOME/bin/mvn" -e deploy:deploy-file \
+					-DgroupId=$groupId -DartifactId=$artifactId -Dversion=$version \
+					-Dfile="$file" -Dpackaging=jar -Dclassifier=sources -DgeneratePom=false \
+					-Durl=$deploy_url -DrepositoryId=$server_id
+				;;
+			* )
+				echo "ERROR - $file upload failed: $status"
+				exit 1
+				;;
+		esac
+	fi
+done
+
+# Google Repository
+list=$(find "$ANDROID_HOME/extras/google/m2repository" -type f -name "*.pom")
+
+
+# android
+#	android
+#	support
+#	test
+# com.android.future:usb
+# com.android.support:appcompat-v7
+# com.android.support:cardview-v7
+# com.android.support:gridlayout-v7
+# com.android.support:leanback-v7
+# com.android.support:mediarouter-v7
+# com.android.support:palette-v7
+# com.android.support:recyclerview-v7
+# com.android.support:support-v13
+# com.android.support:support-v4
+# com.google.android.admob:admob (Deprecated)
+# com.google.android.analytics:analytics (Deprecated)
+# com.google.android.annotations:annotations
+# com.google.android.apk.expansion:play-apk-expansion-downloader
+# com.google.android.gcm:gcm-client (Deprecated)
+# com.google.android.gcm:gcm-server (Deprecated)
+# com.google.android.gdk:gdk
+# com.google.android.gms:google-play-services (Deprecated)
+# com.google.android.gms:google-play-services-for-froyo
+# com.google.android.gms:play-services
+# com.google.android.licensing:play-licensing
+# com.google.android.maps:maps
+
+
+
+
 
