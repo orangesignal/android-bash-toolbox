1f96995825a093f76cbf2aaa8441b3baa5583b58
Koji Sugisawa <sugisawa@kca.biglobe.ne.jp>
2015-01-05 18:43:25 +0900

    Update android-ndk-download.bash

--- android-ndk-download.bash
+++ android-ndk-download.bash
@@ -28,12 +28,19 @@ if [[ -d "$parent_dir/$ndk_dir" ]]; then
 	exit 0
 fi
 
-function install_ndk() {
+archive_paths=()
+
+function installNdk() {
 	local url="$1"
 	local archive_path="${PWD}/${url##*/}"
+
+	if test "$1"='clean'; then
+		rm -f "$archive_path"
+	fi
+
 	if [[ ! -f "$archive_path" ]]; then
 		echo "Downloading Android NDK from $url"
-		local status=$(curl --ipv4 --location --output "$archive_path" --write-out '%{http_code}' $url)
+		local status=$(curl --ipv4 --location --output "$archive_path" --silent --write-out '%{http_code}' $url)
 		if test "$status" != '200'; then
 			echo "ERROR - Download failed: $status"
 			return 1
@@ -42,11 +49,27 @@ function install_ndk() {
 
 	chmod 755 "$archive_path"
 	echo 'all' | "$archive_path"
-#	rm -f "$archive_path"
+	archive_paths=("${archive_paths[@]}" "$archive_path")
 }
 
-install_ndk "http://dl.google.com/android/ndk/android-ndk-r${ndk_ver}-${platform}-x86.bin"
-install_ndk "http://dl.google.com/android/ndk/android-ndk-r${ndk_ver}-${platform}-x86_64.bin"
-if test "$PWD" != "$parent_dir"; then
-	mv "$PWD/$ndk_dir" "$parent_dir"
-fi
+function moveToNdkHomeIfNeed() {
+	if test "$PWD" != "$parent_dir"; then
+		echo "Moving $PWD/$ndk_dir to $parent_dir"
+		mv "$PWD/$ndk_dir" "$parent_dir"
+	fi
+}
+
+function removeArchiveFiles() {
+	echo "Remove archive files..."
+	local i
+	for ((i = 0; i < ${#archive_paths[@]}; i++)) {
+		local archive_path="${archive_paths[i]}"
+		echo "rm -f $archive_path"
+		rm -f "$archive_path"
+	}
+}
+
+installNdk "http://dl.google.com/android/ndk/android-ndk-r${ndk_ver}-${platform}-x86.bin"
+installNdk "http://dl.google.com/android/ndk/android-ndk-r${ndk_ver}-${platform}-x86_64.bin"
+moveToNdkHomeIfNeed
+removeArchiveFiles
